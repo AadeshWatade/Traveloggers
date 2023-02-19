ec3da26d794c69294812f93bb3638fda986f266b
Aadesh <watadeadesh2062001@gmail>
2023-02-19 13:36:50 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, url_for, request, redirect, flash, session, jsonify
+from flask_bcrypt import Bcrypt
+from flask_pymongo import PyMongo
+import random
+import datetime
+from uuid import uuid4
+from flask import request
+import requests
+
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/traveloggers"
+
+mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
+
+
+@app.route("/")
+def helo():
+    return render_template("index.html")
+
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.secret_key = "asdtc"
+    app.run(debug=True)
