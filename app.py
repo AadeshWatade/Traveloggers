bd0e90f823f570f589bd6cd339904f9206bc3fcf
CODERX-69 <66686641+CODERX-69@users.noreply.github.com>
2023-02-22 22:47:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,19 +24,58 @@ def helo():
 def index():
     return render_template("index.html")
 
-@app.route("/signup")
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        Full_Name = request.form['Full_name']
+        Email = request.form['Email']
+        Password = request.form['Password']
+        Confirm_password = request.form['Confirm_password']
+        City = request.form['City']
+        State = request.form['State']
+
+        if Password == Confirm_password:
+            pw_hash = bcrypt.generate_password_hash(Password).decode('utf-8')
+            mongo.db.users.insert_one(
+                {
+                    "fullName": Full_Name,
+                    "email": Email,
+                    "password": pw_hash,
+                    "city": City,
+                    "state": State
+
+                }
+            )
+            flash("Account created Successfully", "success")
+            return redirect(url_for('login'))
+        else:
+            flash('Password and confirm password are not same', 'danger')
     return render_template("signup.html")
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        Email = request.form["Email"]
+        Password = request.form["Password"]
+        found_user = mongo.db.users.find_one({"email": Email})
+        if found_user:
+            if bcrypt.check_password_hash(found_user['password'], request.form['Password']):
+                session['fullName'] = found_user['fullName']
+                session['email'] = found_user['email']
+                flash("Successful Login", "success")
+                return redirect(url_for("index"))
+            else:
+                flash("Login failed. Please try again!", "danger")
+        else:
+            flash("Sorry user not found", "danger")
     return render_template("login.html")
+
 
 @app.route("/add_blog")
 def add_blog():
     return render_template("add_blog.html")
-
-
 
 
 if __name__ == "__main__":
